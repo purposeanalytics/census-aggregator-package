c21fa7f7ee37577f075a3a914325f27e859cdc65
Sharla Gelfand <sharla.gelfand@gmail.com>
2022-05-12 15:50:53 -0400

    Function for getting all census children, without keeping track of whole hierarchy

--- R/get_census_variables_and_children.R
+++ R/get_census_variables_and_children.R
@@ -9,22 +9,65 @@
 #'
 #' @return
 #' @export
-get_census_variables_and_children <- function(dataset = "CA16", regions, level, variables) {
+get_census_variables_and_children <- function(dataset = "CA16",
+                                              # regions, level,
+                                              variables) {
 
   # Check access to internet
   check_internet()
 
-  # Get list of census vectors for given data set
+  # Get census vectors for given dataset
   vectors <- cancensus::list_census_vectors(dataset)
 
-  # Start a list of census vectors and all their children
+  # Get all children vectors for each variable
+  children_vectors <- variables %>%
+    purrr::set_names() %>%
+    purrr::map(~ census_vector_all_children(vectors, .x))
+
+  # Get data for each vector - just do for Canada for now, don't actually set region / level
+  children_vectors %>%
+    purrr::map_dfr(~ get_and_tidy_census_data(dataset = dataset, vectors = .x),
+      .id = "parent_vector"
+    )
+}
+
+census_vector_children <- function(census_vectors, vector) {
+  census_vectors %>%
+    dplyr::filter(parent_vector %in% !!vector) %>%
+    dplyr::pull(vector)
 }
 
 census_vector_is_parent <- function(census_vectors, vector) {
-  vector_children <- census_vectors %>%
-    dplyr::filter(parent_vector == !!vector)
+  vector_children <- census_vector_children(census_vectors, vector)
 
-  is_parent <- nrow(vector_children) > 0
+  length(vector_children) > 0
+}
 
-  is_parent
+census_vector_all_children <- function(census_vectors, vector) {
+  query_vectors <- vector
+  data_vectors <- vector
+
+  while (length(query_vectors) > 0) {
+    children <- census_vector_children(census_vectors, query_vectors)
+
+    data_vectors <- c(data_vectors, children)
+    query_vectors <- children
+  }
+
+  data_vectors
+}
+
+get_and_tidy_census_data <- function(dataset,
+                                     # regions,
+                                     # level,
+                                     vectors,
+                                     labels = "short") {
+  cancensus::get_census(dataset,
+    regions = list(C = "01"),
+    level = "Regions",
+    vectors = vectors,
+    labels = "short"
+  ) %>%
+    dplyr::select(geo_uid = GeoUID, dplyr::all_of(vectors)) %>%
+    pivot_longer(dplyr::all_of(vectors), names_to = "vector")
 }

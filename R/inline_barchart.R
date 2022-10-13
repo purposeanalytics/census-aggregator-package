4e116882d7e7b6af557675b66c23141665390464
Sharla Gelfand <sharla.gelfand@gmail.com>
2022-10-13 13:14:51 -0400

    Scale bars if proportion

--- R/inline_barchart.R
+++ R/inline_barchart.R
@@ -4,7 +4,8 @@ inline_barchart <- function(data, format = "proportion") {
     data <- data %>%
       dplyr::mutate(
         value = .data$value_proportion,
-        value_fmt = scales::percent(.data$value_proportion, accuracy = 0.1)
+        value_fmt = scales::percent(.data$value_proportion, accuracy = 0.1),
+        value = .data$value*100
       )
   } else if (format == "dollar") {
     data <- data %>%
@@ -24,10 +25,8 @@ inline_barchart <- function(data, format = "proportion") {
     dplyr::select(.data$label, .data$hist, .data$value_fmt) %>%
     dplyr::arrange(.data$label)
 
-  # If all values are NA (i.e. all suppressed) need to set scaled = TRUE, to not show bars at all
-  # Faking this a bit - scaling them all down to 0 (out of 100)
-  # Do the same if they are all 0
-  scale_bars <- all(is.na(data[["value_fmt"]]) | data[["hist"]] == 0)
+  # Scale bars if data is a proportion
+  scale_bars <- format == "proportion"
 
   data %>%
     gt::gt() %>%
